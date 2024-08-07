2cfe952206014ecdfd6c74501b2e262178fdb7ef
oluwatobi <oluwatobiajayi001@gmail.com>
2024-08-07 18:18:21 +0100

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/LogIn.jsx 	modified:   src/components/SignUp.jsx

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,23 +1,57 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function LogIn() {
+  //react hook forms.
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  //login form params.
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  function onSubmit() {
+    console.log("login submitted");
+  }
   return (
     <div>
-      <div className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
         <div>
           <div>
             <input
               type="text"
+              {...register("email", { required: true })}
               placeholder="Enter email or username"
+              onChange={(e) => setLoginUsername(e.target.value)}
+              value={loginUsername}
               className="border border-slate-200 bg-slate-200 rounded-full p-3 placeholder:text-black placeholder:opacity-90 w-full text-[18px] placeholder:px-3 focus:outline-green-300"
             />
+            {errors.email && (
+              <span className="text-red-500 font-semibold px-3">
+                This field is required
+              </span>
+            )}
           </div>
         </div>
         <div>
           <div>
             <input
-              type="text"
+              type="password"
+              {...register("password", { required: true })}
               placeholder="Enter password"
+              onChange={(e) => setLoginPassword(e.target.value)}
+              value={loginPassword}
               className="border border-slate-200 bg-slate-200 rounded-full p-3 placeholder:text-black placeholder:opacity-90 w-full text-[18px] placeholder:px-3 focus:outline-green-300"
             />
+            {errors.password && (
+              <span className="text-red-500 font-semibold px-3">
+                This field is required
+              </span>
+            )}
           </div>
         </div>
         <div className="justify-center mt-4 items-center flex border border-transparent border-green-500 text-white p-3 bg-green-500 rounded-full font-bold text-[18px] cursor-pointer hover:opacity-80 duration-300">
@@ -29,7 +63,7 @@ export default function LogIn() {
         <div>
           <hr />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,32 +1,76 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
 export default function SignUp() {
+  //react hook forms params.
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  //signup form params.
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function onSubmit() {
+    console.log("signup submitted");
+  }
+
   return (
     <div>
-      <div className="flex flex-col gap-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
         <div>
           <div>
             <input
-              type="text"
+              type="email"
+              {...register("email", { required: true })}
               placeholder="Enter your email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="border border-slate-200 bg-slate-200 rounded-full p-3 placeholder:text-black placeholder:opacity-90 w-full text-[18px] placeholder:px-3 focus:outline-green-300"
             />
+            {errors.email && (
+              <span className="text-red-500 font-semibold px-3">
+                This field is required
+              </span>
+            )}
           </div>
         </div>
         <div>
           <div>
             <input
               type="text"
+              {...register("username", { required: true })}
               placeholder="Choose username"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
               className="border border-slate-200 bg-slate-200 rounded-full p-3 placeholder:text-black placeholder:opacity-90 w-full text-[18px] placeholder:px-3 focus:outline-green-300"
             />
+            {errors.username && (
+              <span className="text-red-500 font-semibold px-3">
+                This field is required
+              </span>
+            )}
           </div>
         </div>
         <div>
           <div>
             <input
-              type="text"
+              type="password"
+              {...register("password", { required: true })}
               placeholder="Create password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="border border-slate-200 bg-slate-200 rounded-full p-3 placeholder:text-black placeholder:opacity-90 w-full text-[18px] placeholder:px-3 focus:outline-green-300"
             />
+            {errors.password && (
+              <span className="text-red-500 font-semibold px-3">
+                This field is required
+              </span>
+            )}
           </div>
         </div>
         <div className="justify-center mt-4 items-center flex border border-transparent border-green-500 text-white p-3 bg-green-500 rounded-full font-bold text-[18px] cursor-pointer hover:opacity-80 duration-300">
@@ -41,7 +85,7 @@ export default function SignUp() {
         <div>
           <hr />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
